7dbe2138f2f789fa62f16f0f2432a6b8c08c742e
Lucstanislash <52854641+Lucstanislash@users.noreply.github.com>
2022-05-16 14:32:39 +0200

    Update exo2.py

--- exo2.py
+++ exo2.py
@@ -1,4 +1,16 @@
+
+#========================Données Aléatoires==============================
 def Binaire_aleaEx2()
+    a = AleaExAll(2,1,16)
+    b = AleaExAll(2,1,16)
+    signe = randint(1,3) # pour les 3 opérateurs différents, le +, - et *
+    if signe == 1
+        oper = '+'
+    elif signe == 2
+        oper = '-'
+    elif signe == 3
+        oper = '*'
+    return(a,b,oper)
 
 #========================Saisie Manuelle==============================
 
@@ -9,6 +21,7 @@ def Binaire_saisEx2(a, b, oper)
     b = input("Saisissez le second entier binaire de moins de 16 bits : ")
     CtrlSyntaxe(b,16)
 
+#========================Calcul de la réponse selon les données==============================
 def RepExo2 ( a, b, oper)
     if oper == '*'
       rep = ((bin(int(a, 2) * int(b, 2)))
@@ -17,6 +30,8 @@ def RepExo2 ( a, b, oper)
     elif oper == '-'
       rep = ((bin(int(a, 2) - int(b, 2)))
     return(rep)
- 
+
+util = input("Saisissez la réponse au calcul")
+#========================Vérification==============================
 # (rep => réponse calculé) qui est comparé à (util => réponse de l'utilisateur)
  VerifRep(rep,util)
