27d6b1f4b5664e9e04550d2bcd2f6274fb6a26c7
Lucstanislash <52854641+Lucstanislash@users.noreply.github.com>
2022-05-16 15:32:41 +0200

    Create Exercice 7.py

---
+++ Exercice 7.py
@@ -0,0 +1,11 @@
+#========================Données Aléatoires==============================
+def SaisieEx7(ch,sens)
+
+#========================Saisie Manuelle==============================
+def SaisieEx7(ch,sens)
+
+#========================Calcul de la réponse selon les données==============================
+def RepEx7(ch,sens,rep)
+
+#========================Vérification==============================
+VerifRep(rep,util) 
