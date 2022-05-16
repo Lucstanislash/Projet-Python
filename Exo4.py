62892b526ff76b9baaab3f7e3c010af4427b73cf
Dhouriati <105500265+Dhouriati@users.noreply.github.com>
2022-05-16 10:00:31 +0200

    Add files via upload

---
+++ Exo4.py
@@ -0,0 +1,15 @@
+import random
+
+##def Alea_Ex4():
+##    a=0
+##    res=str("") #le resultat
+##    N=random.randrange(1,17) #donne le nombre de bits
+##    for i in range (N):
+##        a=random.randrange(0,2)
+##        res+=str(a)
+##        #print(a)
+##    return (res)
+
+
+def Alea_Ex4():
+    b=0
