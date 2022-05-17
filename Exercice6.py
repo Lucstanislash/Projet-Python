f293ae321cb87197389cbd45944c8f9335449611
m0on11 <105500218+m0on11@users.noreply.github.com>
2022-05-17 11:19:44 +0200

    Update Exercice6.py

--- Exercice6.py
+++ Exercice6.py
@@ -1,16 +1,20 @@
 from random import *
 
+#Utilisateur choisit l’ordre de conversion
 def SaisieOrdreChaine():
     a = input("Saisissez l'ordre de conversion")
     if a == '10' :
         ch1 = input("Saisissez le nombre que vous voulez convertir en base 2 => ")
         CtrlSyntaxe(ch1, 10, 0, 10000)
+        ch3 = input("Saisissez combien de bit après la virgule => ")
     elif a == '2' :
         ch2 = input("Saisissez le nombre que vous voulez convertir en base 10 => ")
         CtrlSyntaxe(ch2, 2, 1, 32)
+        ch3 = input("Saisissez combien de bit après la virgule =>")
     else :
         print('/!\ Erreur : Impossible de convertir sous \nun autre format que 10 ou 2 ')
 
+        
 def AleaOrdre():
     l = [ 'choix1','choix2']
     c = choice(l)
