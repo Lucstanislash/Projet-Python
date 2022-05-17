fd36bd4e12c46bdae50c8915b438d2ec86d4ffa1
m0on11 <105500218+m0on11@users.noreply.github.com>
2022-05-17 11:26:41 +0200

    Update Exercice6.py

--- Exercice6.py
+++ Exercice6.py
@@ -2,12 +2,12 @@ from random import *
 
 #Utilisateur choisit l’ordre de conversion
 def SaisieOrdreChaine():
-    a = input("Saisissez l'ordre de conversion")
-    if a == '10' :
+    basedep = input("Saisissez la base de départ => ")
+    if basedep == '10' :
         ch1 = input("Saisissez le nombre que vous voulez convertir en base 2 => ")
         CtrlSyntaxe(ch1, 10, 0, 10000)
         ch3 = input("Saisissez combien de bit après la virgule => ")
-    elif a == '2' :
+    elif basedep == '2' :
         ch2 = input("Saisissez le nombre que vous voulez convertir en base 10 => ")
         CtrlSyntaxe(ch2, 2, 1, 32)
     else :
