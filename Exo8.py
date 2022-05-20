1b72d1adcac5b38db634e1e9114d65837592abd8
TheBowFire <105500429+TheBowFire@users.noreply.github.com>
2022-05-20 14:33:47 +0200

    Add files via upload

--- Exo8.py
+++ Exo8.py
@@ -7,9 +7,8 @@ def SaisieEx8():
     while a==False:
         TailleTab=int(input("Taille du tableau: "))
         TailleCase=int(input("Taille des cases: "))
-        TailleMot=int(input("Taille des mots: "))
         Ad1mot=int(input("Adresse du premier mot: "))
-        if TailleMot*TailleCase<950:
+        if TailleTab*TailleCase<950:
             a=CtrlSyntaxe(str(TailleTab),10,1,10,30,300)
     return(TailleTab,TailleCase,Ad1mot)
 
@@ -17,10 +16,12 @@ def RepEx8(donne):
     nbmot=donne[0]*donne[1]
     numdermot=(nbmot+donne[2])-1
     numpremot=donne[2]
-    return(nbmot,numpremot,numdermot)
+    return([str(nbmot),str(numpremot),str(numdermot)])
 
 def Exo8():
+    util=[]
     verif=2
+    verif2=2
     man=str(input("Saisie manuel ou aléatoire?"))
     if man == 'manuel':
         donne=SaisieEx8()
@@ -29,11 +30,12 @@ def Exo8():
         TailleCase=AleaExAll(10,1,30)
         Ad1mot=AleaExAll(10,1,1000)
         donne=(TailleTab,TailleCase,Ad1mot)
-        print('Taille du tableau:',TailleTab,'Taille des cases:',TailleCase,'Adresse du premier mot:',Ad1mot)
     rep=RepEx8(donne)
     while verif!=1:
-        util=str(input("Saisir taille du tableau en mot, num du premier mot, num du dernier mot"))
-        verif=VerifRep(str(rep),util)
+        util.append(str(input("Saisir taille du tableau en mot")))
+        util.append(str(input("Saisir adresse du premier mot")))
+        util.append(str(input("Saisir Adresse du dernier mot")))
+        verif=VerifRep(rep,util)
         if verif==0:
             print("réessaye")
         elif verif==-1:
@@ -42,15 +44,15 @@ def Exo8():
     else:
         print("Gagné")
         
-    donne2=AleaExAll(10,donne[2],((donne[0]*donne[1])+donne[2])-1)
+    donne2=AleaExAll(10,1,donne[0])
     print('Donner le contenu de la case',donne2)
     rep2=donne[2]+(donne2-1)*donne[1]
-    while verif!=1:
+    while verif2!=1:
         util=str(input("Saisir la réponse: "))
-        verif=VerifRep(str(rep2),util)
-        if verif==0:
+        verif2=VerifRep(str(rep2),util)
+        if verif2==0:
             print("réessaye")
-        elif verif==-1:
+        elif verif2==-1:
             print("perdu, ","Voici la réponse: ",rep2)
             break
     else:
