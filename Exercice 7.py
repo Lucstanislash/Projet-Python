6a38e8903e1aa97bf3b121ce5d301a3a086b8f07
Adeline311 <105499605+Adeline311@users.noreply.github.com>
2022-05-23 14:00:39 +0200

    Update Exercice 7.py

--- Exercice 7.py
+++ Exercice 7.py
@@ -1,11 +1,87 @@
-#========================Données Aléatoires==============================
-def SaisieEx7(ch,sens)
+from Outils import*
 
-#========================Saisie Manuelle==============================
-def SaisieEx7(ch,sens)
+floatt=-14.21
 
-#========================Calcul de la réponse selon les données==============================
-def RepEx7(ch,sens,rep)
+def Ent_IEEE(floatt):
 
-#========================Vérification==============================
-VerifRep(rep,util) 
+##conversion float vers entier##
+    entier=int(floatt)
+  
+## conversion entier vers binaire##
+    
+    binaire= format(entier,'b')
+    if int(entier)<0:
+        binaire=binaire.replace("-","")    
+
+## on sépare la chaine en 2 le bit implicite dans la première le reste dans l'autre
+    ch_imp=binaire[0]
+    ch_rest=binaire[1:]
+
+## calcul de exposant puis conv vers binaire
+    Exp=127+(len(ch_rest))
+ 
+    ExpB= format(Exp,'b')
+     
+## calcul de la taille de la mantisse pour les décimaux
+    FMantisse= 32-(len(ExpB)+len(binaire))
+
+#transforme la partie décimal d'un float en binaire avec pour longueur FMantisse
+    ch_dec=""
+    dec=str(floatt).replace(str(int(floatt)),'0')
+
+    for i in range(FMantisse):
+        dec=float(dec)*2
+        if int(dec) >= 1:
+            dec-=1
+            ch_dec+='1'
+        else:
+            ch_dec+='0'
+##concaténation des diff chaines 
+    IEEE=ch_imp+ExpB+ch_rest+ch_dec
+    Hexa=Conv(2,16,IEEE)
+    Hexa= Hexa.upper()
+    return(Hexa)
+
+###IEEE=Ent_IEEE(floatt)
+
+
+def IEE_Ent(Hexa):
+
+    IEEE=Conv(16,2,Hexa)
+    
+## récupère  l'exposant en binaire et le bim implicite
+    ExpB=IEEE[1:9]
+    ch_imp=IEEE[0]
+    
+## conversion en entier puis - 127 pour déterminer l'exposant
+    Exp=int(ExpB,2)
+
+    Exp=Exp-127
+
+##récupère les bits de la valeur de l'entier
+    n=Exp+9
+    ch_rest=IEEE[9:n]
+    finIEEE=IEEE[n:]
+
+## reconstitue la chaine en binaire de l'entier puis conversion de entier
+    
+    binaire=ch_imp+ch_rest
+    entier=int(binaire,2)
+    
+    if ch_imp[0]=="1":
+        entier='-'+str(entier)
+
+    cpt=0
+    dec=0
+    for i in finIEEE:
+        cpt-=1
+        if i=='1':
+            dec+=2**cpt
+    dec=round(dec,2)
+    
+    rep=str(dec).replace("0",entier)  
+    return(rep)
+    
+
+
+    
