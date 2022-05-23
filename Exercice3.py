3535fa371ebea1aafb5b1e1eb5fd1fd25bee97c2
m0on11 <105500218+m0on11@users.noreply.github.com>
2022-05-23 14:07:52 +0200

    Update Exercice3.py

--- Exercice3.py
+++ Exercice3.py
@@ -21,37 +21,46 @@ def AleaEx3(min,max):
             ch+=str(a)
 
     return(ch)
-
-
-#=========================== Puissance de 2 ===========================#
-
-power = AleaExAll('puissance', 1, 32)
-
-
+        
 #================== Liste des valeurs binaires aléatoires ===============#
+
+#Exposant
+
+
+def TestTest(n):
+    
+    a = AleaEx3(1,32)
+    
+    if a [-n:] == '0'*n :
+        
+        return(a)
+    
+    else :
+        
+        return(TestTest(n))
+
+
+
+puissance = AleaExAll('puissanceExp',1,16)
 
 #liste qui contient les bonnes réponses
 l = []
 
-def List(l):
+def List(liste, n):
     
     #Crée une liste avec des nombres binaires aléatoires 
 
     for i in range(7): #génère des nombres binaires aléatoires 
         
-        l.append(AleaEx3(1,32))
+        liste.append(AleaEx3(1,32))
 
-    for i in range(3): #pour avoir au moins 3 réponses correctes
+    for i in range(3):#pour avoir au moins 3 réponses correctes
 
-        correct = (int(AleaEx3(1,15),2) * power)
-
-        a = bin(correct)[2:]
-
-        correp = a.zfill(8) 
-
-        l.append(str(correp))
-
-    shuffle(l) #permet de bien mélanger les suggestions
+        a = TestTest(n)
+       
+        liste.append(a)
+        
+    shuffle(liste) #permet de bien mélanger les suggestions
 
 
 #====================== Liste des Réponses correctes ====================#
@@ -59,17 +68,15 @@ def List(l):
 #Liste des réponses correctes
 RepEx3 = []
 
-def RepEx3(l,RepEx3):
+def ReponseEx3(l,RepEx3,n):
     
     #Donne les bonnes réponses
 
     for i in l:
 
-        if int(i,2)% power == 0: 
+        if int(i,2)% (2**n) == 0: 
 
             RepEx3.append(i)
-
-
 
 #====================== Liste des Réponses correctes ====================#
             
@@ -94,5 +101,5 @@ def RepUtil(l,UtilEx3):
 
 VerifRep(RepEx3, UtilEx3)
 
-#Du coup ça marche, si les réponses sont les bonnes = 1
-#sinon = 0
+#ça marche
+   
