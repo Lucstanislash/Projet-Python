6722d2a53736565d0057e6770bf537d64520faed
m0on11 <105500218+m0on11@users.noreply.github.com>
2022-05-24 09:49:11 +0200

    Update Exercice3.py

--- Exercice3.py
+++ Exercice3.py
@@ -2,59 +2,39 @@ from random import *
 from Outils import *
 
 
-def AleaEx3(min,max):
+def AleaEx3(choix,n,mini,maxi):
     
     #Donne des nombres binaires aléatoires
-    #J'utilise cette aléatoire parce que j'ai besoin d'avoir des nombres binaires
-    #qui commencent par 1, sinon une partie du code ne marchera pas
-    
-    mot2='01'
-    
-    ch = '1'
-    
-    alea = randint(min,max)
-    
-    for i in range(alea):
-        
+    ch = '1'        
+    if choix == 'quelconque':
+        taille = randint(mini,maxi)
+        for i in range(taille):
             a=randint(0,1)
-            
             ch+=str(a)
-
+    elif choix == 'correct':
+        taille = randint(min(mini, n+1), maxi)
+        for i in range(taille-n):
+            a=randint(0,1)
+            ch+=str(a)
+        ch+='0'*n # += erreur 
     return(ch)
 
 
+exposant = AleaExAll('puissanceExp',1,16)
 
-#*Erreurs de proba ################## ch[-n:]+=('0'*n) ################# à Tester
-#def TestTest(n):  
-#    a = AleaEx3(1,32)
-#        if a [-n:] == '0'*n :       
-#        return(a)   
-#    else :
-#        return(TestTest(n))
+print("l'exposant est",exposant)
 
-
-
-puissance = AleaExAll('puissanceExp',1,20)
-#================== Liste des valeurs binaires aléatoires ===============#
+    
 #liste qui contient les bonnes réponses
 l = []
 
 def List(liste, n):
-    
     #Crée une liste avec des nombres binaires aléatoires 
-
     for i in range(7): #génère des nombres binaires aléatoires 
-        
-        liste.append(AleaEx3(1,32))
-
+        liste.append(AleaEx3('quelconque',n,1,30))
     for i in range(3):#pour avoir au moins 3 réponses correctes
-
-        a = TestTest(n)
-       
-        liste.append(a)
-        
+        liste.append(AleaEx3('correct',n,1,30))
     shuffle(liste) #permet de bien mélanger les suggestions
-
 
 #====================== Liste des Réponses correctes ====================#
 
@@ -62,13 +42,9 @@ def List(liste, n):
 RepEx3 = []
 
 def ReponseEx3(l,RepEx3,n):
-    
     #Donne les bonnes réponses
-
     for i in l:
-
         if int(i,2)% (2**n) == 0: 
-
             RepEx3.append(i)
 
 #====================== Liste des Réponses correctes ====================#
@@ -80,13 +56,9 @@ UtilEx3 = []
 def RepUtil(l,UtilEx3):
 
     #Retient les réponses de l'utilisateur
-
     repUtil = input('>>> ').lower()
-
     if repUtil in l:
-
         print('Votre réponse est : ', repUtil,'.')#Pour tester voir laquelle des valeurs j'ai ajouté
-
         UtilEx3.append(repUtil)
         
 
@@ -95,4 +67,5 @@ def RepUtil(l,UtilEx3):
 VerifRep(RepEx3, UtilEx3)
 
 #ça marche
+   
    
