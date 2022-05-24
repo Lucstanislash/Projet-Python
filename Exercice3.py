175f175224a51e93bce6f86e830ec25bd39fc122
m0on11 <105500218+m0on11@users.noreply.github.com>
2022-05-24 10:47:15 +0200

    Update Exercice3.py

--- Exercice3.py
+++ Exercice3.py
@@ -19,15 +19,7 @@ def AleaEx3(choix,n,mini,maxi):
         ch+='0'*n # += erreur 
     return(ch)
 
-
-exposant = AleaExAll('puissanceExp',1,16)
-
-print("l'exposant est",exposant)
-
-    
-#liste qui contient les bonnes réponses
-l = []
-
+#Liste des réponses affichées
 def List(liste, n):
     #Crée une liste avec des nombres binaires aléatoires 
     for i in range(7): #génère des nombres binaires aléatoires 
@@ -38,34 +30,13 @@ def List(liste, n):
 
 #====================== Liste des Réponses correctes ====================#
 
-#Liste des réponses correctes
-RepEx3 = []
-
+#Retient les réponses correctes
 def ReponseEx3(l,RepEx3,n):
     #Donne les bonnes réponses
     for i in l:
         if int(i,2)% (2**n) == 0: 
             RepEx3.append(i)
 
-#====================== Liste des Réponses correctes ====================#
-            
-#Liste des réponses de l'utilisateur
-UtilEx3 = []
 
-#Pour tester
-def RepUtil(l,UtilEx3):
-
-    #Retient les réponses de l'utilisateur
-    repUtil = input('>>> ').lower()
-    if repUtil in l:
-        print('Votre réponse est : ', repUtil,'.')#Pour tester voir laquelle des valeurs j'ai ajouté
-        UtilEx3.append(repUtil)
-        
-
-#====================== Vérification des réponses ========================#
-
-VerifRep(RepEx3, UtilEx3)
-
-#ça marche
    
    
