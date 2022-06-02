6ee9df57cb2e692353239cdd667d1edbf73f820c
m0on11 <105500218+m0on11@users.noreply.github.com>
2022-06-02 09:00:20 +0200

    Update Exercice6.py

--- Exercice6.py
+++ Exercice6.py
@@ -1,22 +1,6 @@
 from random import *
 from Outils import *
 
-###=================================== Saisies ===========================================#
-##def SaisieOrdreChaine():
-##    basedep = input("Saisissez la base de départ => ")
-##    if basedep == '10' :
-##        NbDec = input("Saisissez le nombre que vous voulez convertir en base 2 => ")
-##        if CtrlSyntaxe(NbDec, 10, 1,10000) == True:
-##            return(NbDec)
-##        else :
-##            print("/!\ Erreur : Ce n'est pas la bonne synthaxe.")
-##    elif basedep == '2' :
-##        NbBin = input("Saisissez le nombre que vous voulez convertir en base 10 => ")
-##        if CtrlSyntaxe(NbBin, 2, 1, 32) == True:
-##            return(NbBin)
-##        else :
-##            print("/!\ Erreur : Ce n'est pas la bonne synthaxe.")
-
 def ApresVirgule():
     NbBinVirgule = int(input("Saisissez combien de bits après la virgule => "))
     if NbBinVirgule<= 5 :
@@ -41,20 +25,6 @@ def NumVirgule(base):
         entier = uniform(1,10000)
         entier = round(entier,randrange(1,5))
         return str(entier)	
-
-#==============================================================================================  
-
-##def AleaOrdreEx6():
-##    lischoix = [ '10','2']
-##    choix = choice(lischoix)
-##    if choix == '10' :
-##        Dec = 
-##        RepEx6Alea.append(a)
-##        return(a)
-##    else :
-##        b = 
-##        RepEx6Alea.append(b)
-##        return(b)
 
 #==========================================================================#
 #Fonction qui permet de convertir les entiers décimaux en binaire décimaux
@@ -125,35 +95,5 @@ def SaisieEx6():
             ok=False
     return(nb,Ordre)
 
-##def PrincipalEx6():
-##
-##    man=input("manuel ou aléatoire \n")
-##   
-##    if man=="manuel":
-##        saisie=SaisieEx6()
-##
-##        
-##    elif man=="aleatoire":
-##        saisie=AleaEx6()   
-##    else:
-##        print("erreur de syntaxe")
-##        
-##    Ordre=saisie[1]
-##    Val=saisie[0]    
-##    print("Formats de conversion :",Ordre)
-##    print("voici la valeur à convertir :",Val)
-##    
-##    if Ordre == '10' :       
-##        rep = bin_dec(Val)
-##        print("voici la réponse ",rep)
-##        
-##    else:
-##        rep=dec_bin(Val)
-##        print("voici la réponse ",rep)
-##        
-##    util=input("Saisir la réponse")
-##    verif=VerifRep(rep,util)
-##    
-##    return(verif)
 
 
