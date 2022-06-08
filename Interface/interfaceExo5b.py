16f05facc64e2e1b68da1cada41380fc0a9cc3a2
Adeline311 <105499605+Adeline311@users.noreply.github.com>
2022-06-08 09:34:51 +0200

    Add files via upload

--- Interface/interfaceExo5b.py
+++ Interface/interfaceExo5b.py
@@ -454,7 +454,7 @@ Nb.grid(row=5, column=2,ipadx=200,columnspan=4,ipady=15)
                 
 titre=Label(fenetre, text="Conversion", font=("Courier", 40, "italic"), fg='black', bg='lightskyblue1')  
 
-soustitre=Label(fenetre, text="Quelques Indications: valeur 1000000000 impossible à convertir, pas de bits avant 4\n pour les valeurs pas de , mais un .", font=("courier", 20), fg='darkblue', bg='lightskyblue1') 
+soustitre=Label(fenetre, text="Quelques Indications: valeur 1000000000 impossible à convertir\n Attention pas de virgule mais des points                       \n    La valeur absolue doit être comprise entre 1-2^(n-1) et 2^(n-1)-1.", font=("courier", 20), fg='darkblue', bg='lightskyblue1') 
 
 txt1=Label(fenetre, text="Format de départ", font=("courier", 27, "italic"), fg='black', bg='lightskyblue1')
 if man==2:
@@ -479,7 +479,7 @@ else:
 
 
 
-txt3=Label(fenetre, text="Nombre de bits", font=("courier", 27, "italic"), fg='black', bg='lightskyblue1')
+txt3=Label(fenetre, text="Nombre de bits\n(entre 4 et 16)", font=("courier", 27, "italic"), fg='black', bg='lightskyblue1')
 
 
 txt4=Label(fenetre, text="Valeur de départ", font=("courier", 27, "italic"), fg='black', bg='lightskyblue1')
@@ -538,9 +538,9 @@ B2=Button(fenetre, text="Nouveau", state='disabled', font=("courier", 18, "itali
  
 B3=Button(fenetre, text="Valider", font=("courier", 18, "italic"), fg='white', bg='#103985', width=15, height=2,command=lambda:Validation())
  
-B4=Button(fenetre, text="Score", font=("courier", 18, "italic"), fg='white', bg='#103985', width=15, height=2,state='disabled')
+B4=Button(fenetre, text="Menu", font=("courier", 18, "italic"), fg='white', bg='grey', width=15, height=2)
  
-B5=Button(fenetre, text="Quitter", font=("calibri", 18, "bold"), fg='white', bg='grey', width=15, height=2,command=fenetre.destroy)
+B5=Button(fenetre, text="Quitter", font=("calibri", 18, "bold"), fg='white', bg='#103985', width=15, height=2,command=fenetre.destroy)
 
 
 GO=Button(fenetre, text="Go!", font=("calibri", 18, "bold"), fg='white', bg='#103985', width=10, height=0,command=lambda:go())        
@@ -572,7 +572,7 @@ Resultats.grid(row=7, column=2,ipadx=200,columnspan=4,ipady=15)
 B1.grid(row=8, column=1)
 B2.grid(row=8, column=2)
 B3.grid(row=8, column=3)
-B4.grid(row=8, column=4)
-B5.grid(row=8, column=5)
+B4.grid(row=8, column=5)
+B5.grid(row=8, column=4)
 
 fenetre.mainloop()
