1420cc9c29995078043899b0810b90b93a811e93
Dhouriati <105500265+Dhouriati@users.noreply.github.com>
2022-06-24 10:26:35 +0200

    Add files via upload

--- ordo_alea.py
+++ ordo_alea.py
@@ -7,35 +7,43 @@ type=2
 def Nbproc () :
     n=randrange(4,14)
     print("Nombre de processus : ",n)
+    mini=0
+    maxi=0
     cpt=0
     listarv=[]
     listdur=[]
     listp=[]
-    alea=[]
+    alea={}
+    LP=[]
     for i in range (n):
-        cpt=(cpt+1)
         duree=randrange(1,10)
+        cpt=cpt+1
         listdur.append(duree)
-        listp.append(cpt)
-        arv=randrange(1,20)
+        listp.append(i+1)
+        arv=randint(mini,maxi)
         listarv.append(arv)
-        listarv.sort()
+        mini=arv
+        maxi=maxi+duree
+        
+       
+    for i in range(len(listp)):
+        LP.append({"n°":listp[i],"Arv":listarv[i],"duree":listdur[i]})
+         
 
     print("Processus : ",listp)
     print("DateA : ",listarv)
-    print("Quantum : ",listq)
     print("Duree : ",listdur)
-    print(duree)
+    print(LP)
+    
+
+    
     Somme=0
     
-    for i in range(len(listarv)):
-        Somme+=listdur[i] #calcul somme des duree
-        if listarv[i]>Somme: #comparaison somme duree et date arrivee
-            Nbproc()
+##    for i in range(len(listarv)):
+##        Somme+=listdur[i] #calcul somme des duree
+##        if listarv[i]>Somme: #comparaison somme duree et date arrivee
+##            Nbproc()
 
-    alea.append(listdur)
-    alea.append(listarv)
-    alea.append(listp)
     print(alea)
         
 
